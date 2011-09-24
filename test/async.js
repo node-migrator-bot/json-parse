c27aad34d2b162735f739e7d4c3aa09f2a0a7700
muted87 <somicide@gmail.com>
2011-09-24 16:03:03 -0400

    tweak the test file to do just a little bit more to make sure its getting parsed right

--- test/async.js
+++ test/async.js
@@ -3,13 +3,30 @@ json.async("./test.json", function (error, data)
 {
 	if (!error)
 	{
+		console.log("found the file to work with!", JSON.stringify(data));
 		for (var index in data)
 		{
-			console.log(data[index]);
+			var item = data[index];
+			switch (typeof(item))
+			{
+				case "string":
+					var item_type = "hey its a string";
+				break;
+				case "boolean":
+					var item_type = "cool a boolean";
+				break;
+				case "number":
+					var item_type = "wow numbers";
+				break;
+				default:
+					var item_type = "some other crazy type not in the default test suite!";
+				break;
+			}
+			console.log(item_type+": "+item);
 		}
 	}
 	else
 	{
-		console.log("test.json doesnt exist!");
+		console.log("couldnt find a find to start with!");
 	}
 });
